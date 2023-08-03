221332be94b77db45db8fa9f8c24571043c0dcf5
thoriqmacto <thariq.hadad@gmail.com>
2023-08-03 13:52:24 +0700

    Adding API Tests

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var cli = require("./lib/cli");
 var app = {};
 
 // init function
-app.init = function () {
+app.init = function (callback) {
   // Start the server
   server.init();
 
@@ -22,11 +22,14 @@ app.init = function () {
   // Start the CLI, but make sure it starts last
   setTimeout(function () {
     cli.init();
+    callback();
   }, 50);
 };
 
-// Execute
-app.init();
+// Self invoking only if required directly
+if (require.main === module) {
+  app.init(function () {});
+}
 
 // Export the app to outside world
 module.exports = app;
--- test/index.js
+++ test/index.js
@@ -3,6 +3,9 @@
  *
  */
 
+// Override the NODE_ENV variable
+process.env.NODE_ENV = "testing";
+
 // Application logic for the test runner
 _app = {};
 
@@ -11,6 +14,7 @@ _app.tests = {};
 
 // Add on the unit tests
 _app.tests.unit = require("./unit");
+_app.tests.api = require("./api");
 
 // Count all the tests
 _app.countTests = function () {
@@ -100,6 +104,7 @@ _app.produceTestReport = function (limit, successes, errors) {
 
   console.log("");
   console.log("--------END TEST REPORT--------");
+  process.exit(0);
 };
 
 // Run the tests
